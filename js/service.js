a99cb44d0b7aa2a6b155cd0b8e72978480088266
NehaJadhav04 <jadhavneha0412@gmail.com>
2021-09-10 23:42:01 +0530

    made changes

---
+++ js/service.js
@@ -0,0 +1,12 @@
+function showMenu() {
+	var topNav = document.getElementById('topnav');
+	if (topNav.className === "navbar") {
+		topNav.className += " show";
+	} else {
+		topNav.className = "navbar";
+	}
+}
+AOS.init({
+      offset:300,
+      duration:1000
+    });
